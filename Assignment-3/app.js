8b8646b31a032fb5b04a4a52251d23ffa29d9353
Rabia-Bhutto <rabiabhutto05@gmail.com>
2024-05-02 04:51:11 +0500

    First Commit

---
+++ Assignment-3/app.js
@@ -0,0 +1,24 @@
+// Task# 01: Declare a variable called age & assign to it your age. Show your age in an alert box.
+// var age = "20";
+// alert("I am " + age + " years old");
+
+// Task# 02: To be Completed!
+
+// Task# 03: Declare a variable called birthYear & assign to it your birth year. Show the message in your browser:
+// var birthYear = 2003;
+// document.write("My birth year is " + birthYear + "<br>Data type of my declared variable is " + typeof birthYear);
+
+// Task# 04: A visitor visits an online clothing store www.xyzClothing.com . Write a script to store in variables the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+// var customerName = "Walker Moore";
+// var prod1 = "Nike Air Force";
+// var prod2 = "Polo Chukker Shirt(s)";
+// var quantProd1 = "1";
+// var quantProd2 = "2";
+// var storeName = "Urban Bazaar"
+
+// document.write(customerName + " ordered " + quantProd1 + " " + prod1 + " and " + quantProd2 + " " + prod2 + " on " + storeName);
