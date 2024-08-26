9b3ecc1fb201ba30b185515b3ef2500b0d570d2a
Rabia-Bhutto <rabiabhutto05@gmail.com>
2024-08-26 07:59:02 +0500

    Assignment Completed!

---
+++ Assignent-43-48/app.js
@@ -0,0 +1,45 @@
+// Task# 01: Show an alert box on click on a link.
+
+// Done in the HTML file.
+
+
+// Task# 02: Display some Mobile images in browser. On click on an image Show the message in alert to user.
+
+// Done in the HTML file.
+
+
+// Task# 03: Display 10 student records in table and each row should contain a delete    button. If you click on a button to delete a record, entire row should be deleted.
+
+// function deleteRow(button){
+//     button.parentNode.parentNode.remove();
+// }
+
+
+// Task# 04: Display an image in browser. Change the picture on mouseover and set the first picture on mouseout.
+
+// function changeImg(){
+//     document.getElementById('changingImg').src = 'Images/blackhole.jpeg';
+// }
+
+// function resetImg(){
+//     document.getElementById('changingImg').src = 'Images/nasa.png';
+// }
+
+
+// Task# 05: Show a counter in browser. Counter should increase on click on increase button and decrease on click on decrease button. And show updated counter value in browser.
+
+// counter = 0;
+
+// function counterUpdate(){
+//     document.getElementById('counterValue').textContent = counter;
+// }
+
+// function increasing(){
+//     counter++;
+//     counterUpdate();
+// }
+
+// function decreasing(){
+//     counter--;
+//     counterUpdate();
+// }
